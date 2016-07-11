440f3dc13d02bf4eb0f8781e184445aa8ab08e03
Evan Mencke <metaevan@gmail.com>
2016-07-11 09:50:03 -0700

    [Fix] Carousel title clean-up

--- client/app/landing/carousel/carouselCtrl.js
+++ client/app/landing/carousel/carouselCtrl.js
@@ -1,7 +1,9 @@
 app.controller('CarouselController', function ($scope) {
 
-  $scope.carouselInterval = 0;
-  $scope.active = 2;
+  let mainImage = 2;
+
+  $scope.carouselInterval = 1000;
+  $scope.active = mainImage;
   $scope.noWrap = false;
   let imageFiles = [
     "pizza_toppings_1280.jpg",
@@ -14,10 +16,15 @@ app.controller('CarouselController', function ($scope) {
   $scope.slides = [];
 
   for (let i = 0; i < imageFiles.length; i++) {
-    $scope.slides.push({
+    let slide = {
       image: "./assets/images/scaled_1280/"+imageFiles[i],
       id: i
-    })
+    }
+    if (i === mainImage) { 
+      slide.title = "Recipes Tonight!"
+      slide.subtitle = "Browse recipes, store your own, and find them when you want to make something tonight!"
+    }
+    $scope.slides.push(slide)
   }
 
 });
